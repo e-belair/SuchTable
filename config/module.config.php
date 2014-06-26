fd72b512caf367ac321b47d8fe23cb02c23a6d5d
Emmanuel Belair <contact@ebelair.fr>
2014-06-26 13:50:58 +0200

    Add paginationcontrol view scipt

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,11 @@
 <?php
 
 return array(
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
     'view_helpers' => array(
         'invokables' => array(
             'table' => 'SuchTable\View\Helper\Table',
